99ed73ca5263e97f49d7d93ff8c11b871fc52b22
rafaelsantos <santos.rafaelbs@gmail.com>
2019-04-07 20:32:28 -0300

    Creating first application in Go

---
+++ fundamentals/first-application/Hello.go
@@ -0,0 +1,12 @@
+//Main package where applications will be running
+package main
+
+//Importing some other packages
+import "fmt"
+
+//Main function which starts an application
+func main() {
+	fmt.Printf("Hello World %s", "Developer")
+	fmt.Print("!")
+	fmt.Println()
+}
