369ab82af690f41e61a5114cd9fee11ff619c8fa
santosrafaelbs <rafael.balieiro@luizalabs.com>
2019-05-27 13:33:34 -0300

    Understanding the use of make for slices

--- data-structures/slices/Slices.go
+++ data-structures/slices/Slices.go
@@ -22,4 +22,20 @@ func main() {
 
 	slice4 := slice2[:1]
 	fmt.Println(slice2, slice4)
+
+	slice5 := make([]int, 10)
+	slice5[9] = 12
+	fmt.Println(slice5)
+
+	//Create a slice with size of 10 and a array with size of 20
+	//When we create a slice, internally a internal array will be created
+	slice5 = make([]int, 10, 20)
+	fmt.Println(slice5, len(slice5), cap(slice5))
+
+	slice5 = append(slice5, 2, 4, 6, 7, 2, 8, 9, 2, 67, 78)
+	fmt.Println(slice5, len(slice5), cap(slice5))
+
+	//Slices can dynamically increses the size of the internal array
+	slice5 = append(slice5, 34)
+	fmt.Println(slice5, len(slice5), cap(slice5))
 }
