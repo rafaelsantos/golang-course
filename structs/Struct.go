9d4e9c3c0acf513362777c6936f83b15f0b2abed
santosrafaelbs <rafael.balieiro@luizalabs.com>
2019-06-03 13:12:25 -0300

    Working with nested structs

--- structs/Struct.go
+++ structs/Struct.go
@@ -8,9 +8,30 @@ type product struct {
 	discount float64
 }
 
+type item struct {
+	productID int
+	quantity  int
+	price     float64
+}
+
+type order struct {
+	userID int
+	itens  []item
+}
+
 //Creates a receiver of type product
 func (product product) priceWithDiscount() float64 {
 	return product.price * (1 - product.discount)
+}
+
+func (order order) getTotal() float64 {
+	total := 0.0
+
+	for _, item := range order.itens {
+		total += item.price * float64(item.quantity)
+	}
+
+	return total
 }
 
 func main() {
@@ -22,4 +43,15 @@ func main() {
 	}
 
 	log.Println(product1.priceWithDiscount())
+
+	order := order{
+		userID: 1,
+		itens: []item{
+			item{1, 2, 12.10},
+			item{2, 1, 23.49},
+			item{11, 100, 3.24},
+		},
+	}
+
+	log.Println(order.getTotal())
 }
